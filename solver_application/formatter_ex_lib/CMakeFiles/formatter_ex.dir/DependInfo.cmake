4dff5d1938270c8880b83fba27d67167be4b5334
razuwaikin <artyomgorbatenko@gmail.com>
2021-06-12 20:54:27 +0300

    reworked

--- solver_application/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake
+++ solver_application/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake
@@ -1,23 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/raz/razuwaikin/workspace/projects/lab06_homework/solver_application/formatter_ex_lib/formatter_ex.cpp" "/home/raz/razuwaikin/workspace/projects/lab06_homework/solver_application/formatter_ex_lib/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "solver_lib"
-  "formatter_ex_lib"
-  "formatter_ex_lib/formatter_lib"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/berendei/Desktop/lab06/solver_application/formatter_ex_lib/formatter_ex.cpp" "formatter_ex_lib/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o" "gcc" "formatter_ex_lib/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o.d"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/raz/razuwaikin/workspace/projects/lab06_homework/solver_application/formatter_ex_lib/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  "/home/berendei/Desktop/lab06/solver_application/formatter_ex_lib/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
