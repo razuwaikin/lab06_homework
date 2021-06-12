4ea372d90daca1d640b614618e97f239f1b0cc94
razuwaikin <artyomgorbatenko@gmail.com>
2021-06-12 20:30:42 +0300

    temporary files

---
+++ tmp/tests/print.cpp
@@ -0,0 +1,17 @@
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout);
+
+void print(const std::string& text, std::ostream& out)
+{
+  out << text;
+}
+
+void print(const std::string& text, std::ofstream& out)
+{
+  out << text;
+}
